9674e00054cb1b63b7c7d82bb97c51b3a324737c
evgeniyvlee <johnnylee@yandex.ru>
2023-07-08 17:34:37 +0300

    fix: fix checkstyle

--- ewm-main-service/src/main/java/ru/practicum/event/model/Event.java
+++ ewm-main-service/src/main/java/ru/practicum/event/model/Event.java
@@ -1,7 +1,6 @@
 package ru.practicum.event.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
